b5832924e5402a67cf9ca4006990be0e378f2be3
joshua290704 <147586562+joshua290704@users.noreply.github.com>
2026-02-16 16:51:04 +0530

    Iniital commit for pharmacist

---
+++ ClinicManagementMVC/ClinicManagementSystem/Models/Medicine.cs
@@ -0,0 +1,14 @@
+﻿namespace ClinicManagementSystem.Models
+{
+    public class Medicine
+    {
+        public int MedicineId { get; set; }
+        public string MedicineName { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int QuantityStock { get; set; }
+
+        public int MedicineTypeId { get; set; }
+        public MedicineType MedicineType { get; set; }
+    }
+}
---
+++ ClinicManagementMVC/ClinicManagementSystem/Models/MedicineType.cs
@@ -0,0 +1,9 @@
+﻿namespace ClinicManagementSystem.Models
+{
+    public class MedicineType
+    {
+        public int MedicineTypeId { get; set; }
+        public string TypeName { get; set; }
+
+    }
+}
